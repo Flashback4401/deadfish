7073a5c00dd38d28a31e34427b48feb33adcc9ba
DeathWish5862 <email>
2017-08-11 19:20:32 -0700

    gets only audio

--- DF.py
+++ DF.py
@@ -1,16 +1,28 @@
 import mechanize
 from bs4 import BeautifulSoup as BS
+import requests
 url = "https://thepiratebay.org/"
 br = mechanize.Browser()
 br.set_handle_robots(False)
 br.addheaders = [('user-agent', 'Mozilla/5.0 (X11: U; Linux i686; en-US; rv:1.9,2.3) Gecko/20100423 Ubuntu/10.04 (lucid) Firefox/3.6.3'),
                  ('accept', 'text/html, application/xhtml+hml, application/xml; q = 0.9,*/*;q=0.8')]
 br.open(url)
+
 br.select_form(name = "q")
-br["q"] = "taylor swift"
+br["q"] = "pink season"
 res = br.submit()
-content = res.read()
+
+new_link = br.geturl()
+new_link = new_link[:len(new_link) - 1] + '100'
+
+print new_link
+r = requests.get(new_link)
+content = r.text
 br.close()
+
+
+#---------------------------------------------------
+
 soup = BS(content, 'html.parser') #a bs4 object (basically makes it easier to get elements from a site)
 def remove_elements(element_type_string, key, value):
     for element in soup.find_all(element_type_string, {key: value}): #searches the html
@@ -38,7 +50,7 @@ def find_titles():
     for magnet_link in soup.find_all("a", {"title": "Download this torrent using magnet"}): #searches for links
             magnet_links.append(magnet_link.get('href'))
     for count in range(0, len(search_results)):
-        final_search_results[search_results[count]] = magnet_links[count] #merges the titles and links into a dictionary
+        final_search_results[search_results[count] ] = magnet_links[count] #merges the titles and links into a dictionary
     return final_search_results
 #------------------in progress--------------------
 final = find_titles()
@@ -54,7 +66,6 @@ with open("mechanize_results.html", "w") as f:
      html = soup.prettify("utf-8")
      f.write(html)
 
-
 """
 Remove all the un-useful page elements.
 
@@ -62,4 +73,5 @@ Make a algorithm to take all the search result titles in the html and put them i
 
 Put the dictionary trough a bunch of for loops so the best option is chosen. This function should return the 
 best title.
+
 """
